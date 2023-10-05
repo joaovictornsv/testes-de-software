048a5f0b0c27bda176b0477b14a361def8cf1948
João Victor Negreiros <joaovictornsv@gmail.com>
2023-10-05 08:10:27 -0300

    Create corretor-imovel.test.js

---
+++ aula-28-09-23/corretor-imovel.test.js
@@ -0,0 +1,76 @@
+const { Imovel, Corretor } = require(".")
+
+describe('Imovel', () => {
+  test('tipo CASA - oportunidade TRUE', () => {
+    const imovel = new Imovel()
+    imovel.codigo = 1
+    imovel.tipo = 'CASA'
+    imovel.tamanho = 50
+    imovel.preco = 100_000
+    expect(imovel.oportunidade()).toEqual(true)
+  })
+  test('tipo CASA - oportunidade FALSE', () => {
+    const imovel = new Imovel()
+    imovel.codigo = 1
+    imovel.tipo = 'CASA'
+    imovel.tamanho = 50
+    imovel.preco = 300_000
+    expect(imovel.oportunidade()).toEqual(false)
+  })
+  test('tipo AP - oportunidade TRUE', () => {
+    const imovel = new Imovel()
+    imovel.codigo = 1
+    imovel.tipo = 'AP'
+    imovel.tamanho = 50
+    imovel.preco = 100_000
+    expect(imovel.oportunidade()).toEqual(true)
+  })
+  test('tipo AP - oportunidade FALSE', () => {
+    const imovel = new Imovel()
+    imovel.codigo = 1
+    imovel.tipo = 'AP'
+    imovel.tamanho = 50
+    imovel.preco = 300_000
+    expect(imovel.oportunidade()).toEqual(false)
+  })
+})
+
+const criarImovelOportunidade = () => {
+  const imovel = new Imovel()
+  imovel.codigo = 1
+  imovel.tipo = 'AP'
+  imovel.tamanho = 50
+  imovel.preco = 100_000
+  return imovel
+}
+
+const criarImovelSemOportunidade = () => {
+  const imovel = new Imovel()
+  imovel.codigo = 1
+  imovel.tipo = 'AP'
+  imovel.tamanho = 50
+  imovel.preco = 300_000
+  return imovel
+}
+
+describe('Corretor', () => {
+  test('vendedorEstrela TRUE', () => {
+    const corretor = new Corretor()
+    corretor.CRECI = 12345
+    corretor.nome = 'Cabecinha'
+
+    corretor.adicionaImovel(criarImovelOportunidade())
+    corretor.adicionaImovel(criarImovelSemOportunidade())
+
+    expect(corretor.vendedorEstrela()).toEqual(true)  
+  })
+  test('vendedorEstrela FALSE', () => {
+    const corretor = new Corretor()
+    corretor.CRECI = 12345
+    corretor.nome = 'Cabecinha'
+
+    corretor.adicionaImovel(criarImovelSemOportunidade())
+
+    expect(corretor.vendedorEstrela()).toEqual(false)
+  })
+})
